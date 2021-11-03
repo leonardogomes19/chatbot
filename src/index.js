5234778722ae9c56a17123b0ab391919faf7c53c
Leonardo Ribeiro <leogomesribeiro2017@gmail.com>
2021-11-03 03:07:24 -0300

    att

--- src/index.js
+++ src/index.js
@@ -293,10 +293,10 @@ app.post("/whatsapp", async (req, res) => {
 
           res.header("Content-Type", "text/xml").status(200);
           results.body(
-            "CIÊNCIAS AMBIENTAIS - ACADÊMICO https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/mestrado-academico-em-ciencias-ambientais/" +
-            "\nCIÊNCIAS AMBIENTAIS - PROFISSIONAL https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/mestrado-profissional-em-ciencias-ambientais/" +
-            "\nCIÊNCIAS AMBIENTAIS - PROFISSIONAL (TURMA ESPECIAL DE IMPERATRIZ-MA) https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/mestrado-profissional-em-ciencias-ambientais-turma-especial-imperatriz-ma/" +
-            "\nCIÊNCIAS DA SAÚDE - ACADÊMICO | NOVO https://unitau.br/cursos/pos-graduacao/odontologia/mestrado-academico-em-ciencias-da-saude/" +
+            "CIÊNCIAS AMBIENTAIS - ACADÊMICO https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/mestrado-academico-em-ciencias-ambientais/\n" +
+            "\nCIÊNCIAS AMBIENTAIS - PROFISSIONAL https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/mestrado-profissional-em-ciencias-ambientais/\n" +
+            "\nCIÊNCIAS AMBIENTAIS - PROFISSIONAL (TURMA ESPECIAL DE IMPERATRIZ-MA) https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/mestrado-profissional-em-ciencias-ambientais-turma-especial-imperatriz-ma/\n" +
+            "\nCIÊNCIAS DA SAÚDE - ACADÊMICO | NOVO https://unitau.br/cursos/pos-graduacao/odontologia/mestrado-academico-em-ciencias-da-saude/\n" +
             "\nODONTOLOGIA - ACADÊMICO https://unitau.br/cursos/pos-graduacao/odontologia/mestrado-em-odontologia/" 
 
           );
@@ -321,11 +321,11 @@ app.post("/whatsapp", async (req, res) => {
 
           res.header("Content-Type", "text/xml").status(200);
           results.body(
-            "DESENVOLVIMENTO HUMANO - ACADÊMICO https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/mestrado-em-desenvolvimento-humano/" +
-            "\nEDUCAÇÃO - PROFISSIONAL https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/mestrado-profissional-em-educacao/" +
-            "\nGESTÃO E DESENVOLVIMENTO REGIONAL - PROFISSIONAL (MGDR) https://unitau.br/cursos/pos-graduacao/gestao-e-negocios/mestrado-profissional-em-gestao-e-desenvolvimento-regional-mgdr/" +
-            "\nGESTÃO E DESENVOLVIMENTO REGIONAL - PROFISSIONAL (PARCERIA UNITAU-FACIBI - REGIÃO DE IBIAPABA-CE) https://unitau.br/cursos/pos-graduacao/gestao-e-negocios/gestao-e-desenvolvimento-regional-profissional-parceria-unitau-facibi-regiao-de-ibiapaba-ce/" +
-            "\nLINGUÍSTICA APLICADA - ACADÊMICO https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/mestrado-em-linguistica-aplicada/" +
+            "DESENVOLVIMENTO HUMANO - ACADÊMICO https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/mestrado-em-desenvolvimento-humano/\n" +
+            "\nEDUCAÇÃO - PROFISSIONAL https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/mestrado-profissional-em-educacao/\n" +
+            "\nGESTÃO E DESENVOLVIMENTO REGIONAL - PROFISSIONAL (MGDR) https://unitau.br/cursos/pos-graduacao/gestao-e-negocios/mestrado-profissional-em-gestao-e-desenvolvimento-regional-mgdr/\n" +
+            "\nGESTÃO E DESENVOLVIMENTO REGIONAL - PROFISSIONAL (PARCERIA UNITAU-FACIBI - REGIÃO DE IBIAPABA-CE) https://unitau.br/cursos/pos-graduacao/gestao-e-negocios/gestao-e-desenvolvimento-regional-profissional-parceria-unitau-facibi-regiao-de-ibiapaba-ce/\n" +
+            "\nLINGUÍSTICA APLICADA - ACADÊMICO https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/mestrado-em-linguistica-aplicada/\n" +
             "\nPLANEJAMENTO E DESENVOLVIMENTO REGIONAL - ACADÊMICO (MPDR) https://unitau.br/cursos/pos-graduacao/gestao-e-negocios/mestrado-academico-em-planejamento-e-desenvolvimento-regional-mpdr/" 
 
           );
@@ -352,7 +352,7 @@ app.post("/whatsapp", async (req, res) => {
 
           res.header("Content-Type", "text/xml").status(200);
           results.body(
-            "CIÊNCIAS DA SAÚDE - ACADÊMICO https://unitau.br/cursos/pos-graduacao/odontologia/ciencias-da-saude/" +
+            "CIÊNCIAS DA SAÚDE - ACADÊMICO https://unitau.br/cursos/pos-graduacao/odontologia/ciencias-da-saude/\n" +
             "\nODONTOLOGIA - ACADÊMICO https://unitau.br/cursos/pos-graduacao/odontologia/doutorado-em-odontologia/" 
           );
   
@@ -379,7 +379,7 @@ app.post("/whatsapp", async (req, res) => {
 
           res.header("Content-Type", "text/xml").status(200);
           results.body(
-            "DESENVOLVIMENTO REGIONAL - INTERINSTITUCIONAL UNITAU E UNISC https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/doutorado-interinstitucional-em-desenvolvimento-regional/" +
+            "DESENVOLVIMENTO REGIONAL - INTERINSTITUCIONAL UNITAU E UNISC https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/doutorado-interinstitucional-em-desenvolvimento-regional/\n" +
             "\nEDUCAÇÃO - INTERINSTITUCIONAL UNITAU E ESTÁCIO DE SÁ - RJ (UNESA) https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/doutorado-interinstitucional-em-educacao-parceria-unitau-e-estacio-de-sa-rj-unesa/" 
           );
   
@@ -408,17 +408,17 @@ app.post("/whatsapp", async (req, res) => {
           res.header("Content-Type", "text/xml").status(200);
           results.body(
             "ENGENHARIA\n\n" +
-            "\NENERGIA SOLAR FOTOVOLTAICA - TECNOLOGIAS E SISTEMAS z https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/energia-solar-fotovoltaica-tecnologias-e-sistemas/" +
-            "\nENGENHARIA DA QUALIDADE LEAN SEIS SIGMA GREEN BELT https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/engenharia-da-qualidade-lean-seis-sigma-green-belt-sabado/" +
-            "\nENGENHARIA DE SEGURANÇA DO TRABALHO https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/engenharia-de-seguranca-do-trabalho/" +
-            "\nMANUTENÇÃO INDUSTRIAL https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/manutencao-industrial/" +
+            "\NENERGIA SOLAR FOTOVOLTAICA - TECNOLOGIAS E SISTEMAS z https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/energia-solar-fotovoltaica-tecnologias-e-sistemas/\n" +
+            "\nENGENHARIA DA QUALIDADE LEAN SEIS SIGMA GREEN BELT https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/engenharia-da-qualidade-lean-seis-sigma-green-belt-sabado/\n" +
+            "\nENGENHARIA DE SEGURANÇA DO TRABALHO https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/engenharia-de-seguranca-do-trabalho/\n" +
+            "\nMANUTENÇÃO INDUSTRIAL https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/manutencao-industrial/\n" +
             "\nPROJETO MECÂNICO, EAD https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/projeto-mecanico-na-modalidade-a-distancia/"
           );
   
           results2.body(
             "TECNOLOGIA\n\n" +
-            "\nANÁLISE DE DADOS E BIG DATA https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/analise-de-dados-e-big-data/" +
-            "\nAUTOMAÇÃO E CONTROLE INDUSTRIAL - MECATRÔNICA https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/automacao-e-controle-industrial-mecatronica/" +
+            "\nANÁLISE DE DADOS E BIG DATA https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/analise-de-dados-e-big-data/\n" +
+            "\nAUTOMAÇÃO E CONTROLE INDUSTRIAL - MECATRÔNICA https://unitau.br/cursos/pos-graduacao/engenharia-mecanica/automacao-e-controle-industrial-mecatronica/\n" +
             "\nCLOUD COMPUTING https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/cloud-computing/" 
           );
 
@@ -445,25 +445,27 @@ app.post("/whatsapp", async (req, res) => {
           res.header("Content-Type", "text/xml").status(200);
           results.body(
             "AMBIENTE\n\n" +
-            "\NAPICULTURA E MELIPONICULTURA - EAD https://unitau.br/cursos/pos-graduacao/ead/apicultura-e-meliponicultura/" +
-            "\nDESIGN DE ASSENTAMENTOS SUSTENTÁVEIS E ECOVILAS - EAD https://unitau.br/cursos/pos-graduacao/arquitetura/design-de-assentamentos-sustentaveis-e-ecovilas/" +
-            "\nEMPREENDEDORISMO SOCIOAMBIENTAL - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/empreendedorismo-socioambiental/" +
+            "\NAPICULTURA E MELIPONICULTURA - EAD https://unitau.br/cursos/pos-graduacao/ead/apicultura-e-meliponicultura/\n" +
+            "\nDESIGN DE ASSENTAMENTOS SUSTENTÁVEIS E ECOVILAS - EAD https://unitau.br/cursos/pos-graduacao/arquitetura/design-de-assentamentos-sustentaveis-e-ecovilas/\n" +
+            "\nEMPREENDEDORISMO SOCIOAMBIENTAL - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/empreendedorismo-socioambiental/\n" +
             "\nGESTÃO DE RESÍDUOS INDUSTRIAIS, URBANOS E RURAIS https://unitau.br/cursos/pos-graduacao/ciencias-agrarias/gestao-de-residuos-industriais-urbanos-e-rurais/" 
           );
   
           results2.body(
             "SAÚDE\n\n" +
-            "\nALIMENTAÇÃO ESCOLAR - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/alimentacao-escolar/" +
-            "\nANÁLISES CLÍNICAS https://unitau.br/cursos/pos-graduacao/biologia/analises-clinicas/" +
-            "\nANESTESIA CARDIOVASCULAR E TORÁCICA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/anestesia-cardiovascular-e-toracica/" +
-            "\nAVALIAÇÃO, CONTROLE, REGULAÇÃO E AUDITORIA EM SAÚDE https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/avaliacao-controle-regulacao-e-auditoria-em-saude/" +
-            "\nBIOLOGIA MOLECULAR - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/biologia-molecular/" +
-            "\nDIAGNÓSTICO LABORATORIAL MULTIDISCIPLINAR - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/diagnostico-laboratorial-ndash-multidisciplinar/" +
-            "\nDOR: ABORDAGEM INTERDISCIPLINAR - EAD https://unitau.br/cursos/pos-graduacao/fisioterapia/dor-abordagem-interdisciplinar/" +
-            "\nHARMONIZAÇÃO OROFACIAL https://unitau.br/cursos/pos-graduacao/odontologia/harmonizacao-orofacial/" +
-            "\nIMPLANTODONTIA https://unitau.br/cursos/pos-graduacao/odontologia/implantodontia/" +
-            "\nINTERDISCIPLINARIDADE DAS POLÍTICAS DE ASSISTÊNCIA SOCIAL, EDUCAÇÃO E SAÚDE NA INTERFACE DO TRABALHO COM FAMÍLIAS https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/interdisciplinaridade-das-politicas-de-assistencia-social-educacao-e-saude-na-interface-do-trabalho-com-familias/" +
-            "\nINTERVENÇÃO FAMILIAR: PSICOTERAPIA, ORIENTAÇÃO E MEDIAÇÃO DE CONFLITOS https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/intervencao-familiar-psicoterapia-orientacao-e-mediacao-de-conflitos/" +
+            "\nALIMENTAÇÃO ESCOLAR - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/alimentacao-escolar/\n" +
+            "\nANÁLISES CLÍNICAS https://unitau.br/cursos/pos-graduacao/biologia/analises-clinicas/\n" +
+            "\nANESTESIA CARDIOVASCULAR E TORÁCICA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/anestesia-cardiovascular-e-toracica/\n" +
+            "\nAVALIAÇÃO, CONTROLE, REGULAÇÃO E AUDITORIA EM SAÚDE https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/avaliacao-controle-regulacao-e-auditoria-em-saude/\n" +
+            "\nBIOLOGIA MOLECULAR - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/biologia-molecular/\n" +
+            "\nDIAGNÓSTICO LABORATORIAL MULTIDISCIPLINAR - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/diagnostico-laboratorial-ndash-multidisciplinar/\n" +
+            "\nDOR: ABORDAGEM INTERDISCIPLINAR - EAD https://unitau.br/cursos/pos-graduacao/fisioterapia/dor-abordagem-interdisciplinar/\n" +
+            "\nHARMONIZAÇÃO OROFACIAL https://unitau.br/cursos/pos-graduacao/odontologia/harmonizacao-orofacial/\n" +
+            "\nIMPLANTODONTIA https://unitau.br/cursos/pos-graduacao/odontologia/implantodontia/\n" +
+            "\nINTERDISCIPLINARIDADE DAS POLÍTICAS DE ASSISTÊNCIA SOCIAL, EDUCAÇÃO E SAÚDE NA INTERFACE DO TRABALHO COM FAMÍLIAS https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/interdisciplinaridade-das-politicas-de-assistencia-social-educacao-e-saude-na-interface-do-trabalho-com-familias/\n\n" +
+            "\nPara encontrar mais cursos, acesse o link: https://unitau.br/pos-graduacao/"
+
+/*             "\nINTERVENÇÃO FAMILIAR: PSICOTERAPIA, ORIENTAÇÃO E MEDIAÇÃO DE CONFLITOS https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/intervencao-familiar-psicoterapia-orientacao-e-mediacao-de-conflitos/" +
             "\nMEDICINA DO TRABALHO https://unitau.br/cursos/pos-graduacao/medicina/medicina-do-trabalho/" +
             "\nMEDICINA DO TRABALHO - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/medicina-do-trabalho/" +
             "\nMETABOLISMO, PRÁTICA E TERAPIA NUTRICIONAL - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/metabolismo-pratica-e-terapia-nutricional/" +
@@ -483,7 +485,7 @@ app.post("/whatsapp", async (req, res) => {
             "\nSAÚDE ESTÉTICA https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/saude-estetica/" +
             "\nSAÚDE MENTAL NO TRABALHO - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/saude-mental-no-trabalho/" +
             "\nSEGURANÇA SANITÁRIA EM SERVIÇOS DE ALIMENTAÇÃO COLETIVA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/seguranca-sanitaria-em-servicos-de-alimentacao-coletiva/" +
-            "\nTRANSTORNO DO ESPECTRO AUTISTA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/transtorno-do-espectro-autista/" 
+            "\nTRANSTORNO DO ESPECTRO AUTISTA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/transtorno-do-espectro-autista/"  */
           );
 
           results3.body(
@@ -511,32 +513,32 @@ app.post("/whatsapp", async (req, res) => {
           res.header("Content-Type", "text/xml").status(200);
           results.body(
             "ADMINISTRAÇÃO E NEGÓCIOS\n\n" +
-            "\nGESTÃO PÚBLICA MUNICIPAL https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/gestao-publica-municipal/" +
+            "\nGESTÃO PÚBLICA MUNICIPAL https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/gestao-publica-municipal/\n" +
             "\nLIDERANÇA E COACHING APLICADOS A GESTÃO DO CAPITAL HUMANO - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/lideranca-e-coaching-aplicados-a-gestao-do-capital-humano/" 
           );
 
           results2.body(
             "COMUNICAÇÃO E MARKETING\n\n" +
-            "\nDESIGN E CRIAÇÃO DIGITAL https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/design-e-criacao-digital/" +
+            "\nDESIGN E CRIAÇÃO DIGITAL https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/design-e-criacao-digital/\n" +
             "\nMARKETING DIGITAL 4.0 https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/marketing-digital-4-0/" 
           );
 
           results3.body(
             "DIREITO\n\n" +
-            "\NDIREITO DO TRABALHO, PROCESSUAL DO TRABALHO E SEGURIDADE SOCIAL: TEORIA E PRÁTICA DA REFORMA TRABALHISTA E DO DIREITO DO TRABALHO NA PANDEMIA - EAD  https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/direito-do-trabalho-processual-do-trabalho-e-seguridade-social-teoria-e-pratica-da-reforma-trabalhista-e-do-direito-do-trabalho-na-pandemia/" 
+            "\nDIREITO DO TRABALHO, PROCESSUAL DO TRABALHO E SEGURIDADE SOCIAL: TEORIA E PRÁTICA DA REFORMA TRABALHISTA E DO DIREITO DO TRABALHO NA PANDEMIA - EAD  https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/direito-do-trabalho-processual-do-trabalho-e-seguridade-social-teoria-e-pratica-da-reforma-trabalhista-e-do-direito-do-trabalho-na-pandemia/" 
           );
   
           results4.body(
             "EDUCAÇÃO\n\n" +
-            "\nEDUCAÇÃO ESPECIAL E INCLUSIVA https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/alimentacao-escolar/"+
-            "\nEDUCAÇÃO MUSICAL  https://unitau.br/cursos/pos-graduacao/biologia/analises-clinicas/" +
-            "\nFERRAMENTAS DIGITAIS PARA DOCENTES - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/anestesia-cardiovascular-e-toracica/" +
-            "\nGESTÃO E DOCÊNCIA DO ENSINO SUPERIOR - EAD  https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/avaliacao-controle-regulacao-e-auditoria-em-saude/" +
-            "\nGESTÃO EDUCACIONAL INOVADORA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/biologia-molecular/" +
-            "\nGESTÃO PEDAGÓGICA EMPRESARIAL E EDUCAÇÃO CORPORATIVA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/diagnostico-laboratorial-ndash-multidisciplinar/" +
-            "\nMETODOLOGIAS ATIVAS DE APRENDIZAGEM E ENSINO HÍBRIDO - EAD https://unitau.br/cursos/pos-graduacao/fisioterapia/dor-abordagem-interdisciplinar/" +
-            "\nPSICOLOGIA E SERVIÇO SOCIAL NA EDUCAÇÃO BÁSICA - EAD https://unitau.br/cursos/pos-graduacao/odontologia/harmonizacao-orofacial/" +
-            "\nPSICOPEDAGOGIA https://unitau.br/cursos/pos-graduacao/odontologia/implantodontia/" +
+            "\nEDUCAÇÃO ESPECIAL E INCLUSIVA https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/alimentacao-escolar/\n"+
+            "\nEDUCAÇÃO MUSICAL  https://unitau.br/cursos/pos-graduacao/biologia/analises-clinicas/\n" +
+            "\nFERRAMENTAS DIGITAIS PARA DOCENTES - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/anestesia-cardiovascular-e-toracica/\n" +
+            "\nGESTÃO E DOCÊNCIA DO ENSINO SUPERIOR - EAD  https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/avaliacao-controle-regulacao-e-auditoria-em-saude/\n" +
+            "\nGESTÃO EDUCACIONAL INOVADORA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/biologia-molecular/\n" +
+            "\nGESTÃO PEDAGÓGICA EMPRESARIAL E EDUCAÇÃO CORPORATIVA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/diagnostico-laboratorial-ndash-multidisciplinar/\n" +
+            "\nMETODOLOGIAS ATIVAS DE APRENDIZAGEM E ENSINO HÍBRIDO - EAD https://unitau.br/cursos/pos-graduacao/fisioterapia/dor-abordagem-interdisciplinar/\n" +
+            "\nPSICOLOGIA E SERVIÇO SOCIAL NA EDUCAÇÃO BÁSICA - EAD https://unitau.br/cursos/pos-graduacao/odontologia/harmonizacao-orofacial/\n" +
+            "\nPSICOPEDAGOGIA https://unitau.br/cursos/pos-graduacao/odontologia/implantodontia/\n" +
             "\nPSICOPEDAGOGIA - EAD https://unitau.br/cursos/pos-graduacao/pesquisa-e-pos-graduacao/interdisciplinaridade-das-politicas-de-assistencia-social-educacao-e-saude-na-interface-do-trabalho-com-familias/" 
           );
 
